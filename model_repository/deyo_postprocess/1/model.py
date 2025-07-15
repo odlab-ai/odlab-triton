2bb5a3bc444757408311d9d807a84a0877df5785
tien-ngnvan <tien.ngnvan@gmail.com>
2025-07-15 15:31:36 +0700

    Merge deyo branch and refactor

--- model_repository/deyo_postprocess/1/model.py
+++ model_repository/deyo_postprocess/1/model.py
@@ -2,6 +2,8 @@ import json
 import numpy as np
 import triton_python_backend_utils as pb_utils
 
+
+
 class TritonPythonModel:
     def initialize(self, args):
         self.model_config = json.loads(args["model_config"])
